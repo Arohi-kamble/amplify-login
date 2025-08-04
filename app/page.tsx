2f15a701dcc86ef5c8ed9c34458157ecf8d6581f
Arohi Kamble <arohi.kamble@dharamcs.com>
2025-08-04 10:58:57 +0530

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-'use client';
-import { useState, FormEvent, ChangeEvent } from 'react';
-import { generateClient } from 'aws-amplify/data';
-import { Schema } from '@/amplify/data/resource'; // adjust path to your actual schema
+"use client";
+import { Amplify } from "aws-amplify";
+import { useState } from "react";
+import { generateClient } from "aws-amplify/data";
+import { type Schema } from "@/amplify/data/resource";
 
+import outputs from "@/amplify_outputs.json";
+
+   Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function Home() {
